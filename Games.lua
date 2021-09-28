fdf33e681a9919a825975cc6f929020553a64058
Nameless <Nameless9000@protonmail.com>
2021-09-28 23:14:56 +0100

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,5 +1,6 @@
 -- this is if someone wants to make a hub out of my scripts lol
 return {
+  ["PopItTradingDupe.lua"] = {7346416636},
   ["ElementalGrindGameAutofarm.lua"] = {4375458071},
   ["CubeEatCubeAutofarm.lua"] = {248409235}
 }

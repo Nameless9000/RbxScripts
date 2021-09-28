e540aab35cfc63a41c2f5dc8c4753a03b444b293
Nameless <Nameless9000@protonmail.com>
2021-09-28 23:14:31 +0100

    Create PopItTradingDupe.lua

---
+++ PopItTradingDupe.lua
@@ -0,0 +1,92 @@
+shared.item = shared.item or "Gummy Rainbow" -- change to item u want to duper
+
+shared.timing = 0.085 or shared.timing -- change to 0.05 - 0.3 depending on wifi speed
+
+repeat wait() until game:IsLoaded()
+wait(2)
+if game.PlaceId == 7346416636 then
+    if not game.Players:FindFirstChild(shared.altName or "*") and #game.Players:GetChildren() > 7 then
+        game:Shutdown()
+    end
+    
+    local success, errorMessage = pcall(function()
+        
+    game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child)
+    if child.Name == 'ErrorPrompt' and child:FindFirstChild('MessageArea') and child.MessageArea:FindFirstChild("ErrorFrame") then
+        game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId)
+    end
+end)
+
+    local TeleportService = game:GetService("TeleportService")
+    local lplr = game.Players.LocalPlayer
+
+function dupe()
+    wait(0.2)
+    game:GetService("ReplicatedStorage").RemoteEvents.Jumped:FireServer()
+    if shared.timing then wait(shared.timing) end
+    game.Players.LocalPlayer:Kick("DUPED")
+end
+
+
+    function getBoard(plr)
+        for i,v in pairs(game:GetService("Workspace").Boards:GetChildren()) do
+            local plr1 = v.Player1.Value
+            local plr2 = v.Player2.Value
+            if plr then
+                if plr1 == plr or plr2 == plr then
+                    return v
+                end
+            else
+                if plr1 == lplr or plr2 == lplr then
+                    return v
+                end
+            end
+        end
+    end
+
+    function getEmptyBoard()
+        for i,v in pairs(game:GetService("Workspace").Boards:GetChildren()) do
+            local plr1 = v.Player1.Value
+            local plr2 = v.Player2.Value
+            if plr then
+                if plr1 == nil or plr2 == nil then
+                    lplr.Character:MoveTo(v.PrimaryPart.Position)
+                end
+            else
+                if plr1 == nil or plr2 == nil then
+                    lplr.Character:MoveTo(v.PrimaryPart.Position)
+                end
+            end
+        end
+    end
+
+    function autodrop(b)
+        for i = 1,b do
+            game:GetService("ReplicatedStorage").RemoteEvents.Drop:FireServer(shared.item)
+            wait(0.5)
+            game:GetService("ReplicatedStorage").RemoteEvents.Equip:FireServer(shared.item)
+            wait(0.5)
+            game:GetService("ReplicatedStorage").RemoteEvents.Drop:FireServer(shared.item)
+        end
+    end
+
+    function autodupe()
+        getEmptyBoard()
+        wait(2)
+        local brd = getBoard()
+        local x = brd:WaitForChild("Controls").Close.Pad
+        wait()
+        lplr.Character:MoveTo(x.Position)
+        autodrop(15)
+        wait(0.2)
+        lplr.Character:MoveTo(x.Position)
+        wait(0.05)
+        dupe()
+    end
+    autodupe()
+    end)
+
+    if success then
+        warn("No errors")
+    end
+end

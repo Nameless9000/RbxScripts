9f8023cec92113331c0acec4be0152cef26d30d7
Nameless <Nameless9000@protonmail.com>
2021-10-09 22:14:24 +0100

    Update SetDate.lua

--- StockRise/SetDate.lua
+++ StockRise/SetDate.lua
@@ -1,4 +1,5 @@
--- lol
+-- https://v3rmillion.net/showthread.php?tid=1140358
+-- https://www.roblox.com/games/6266940721/StockRise-BETA
 
 local Day = shared.day or 1
 local Month = shared.month or "January"

30231e383ce9eed8cab6b170382f201fc238233a
Nameless <Nameless9000@protonmail.com>
2021-10-09 22:05:02 +0100

    Create InfiniteExp.lua

---
+++ StockRise/InfiniteExp.lua
@@ -0,0 +1,12 @@
+--[[ how to use:
+  1. run script
+  2. buy stock (if u don't have any)
+  3. wait for stock to go up
+  4. sell stock
+  5. inf exp given rejoin for it to save
+]]
+
+game:GetService("RunService").RenderStepped:Connect(function()
+    local x = math.random(999999,999999999)
+    game:GetService("ReplicatedStorage").PortfolioFunctions.Experience:InvokeServer(x, 0)
+end)

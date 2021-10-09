0027d1160359dc67f173d4df5f456a25af47781a
Nameless <Nameless9000@protonmail.com>
2021-10-09 20:12:23 +0100

    Create AdvancedLauncherTycoonDestroyer.lua

---
+++ AdvancedLauncherTycoonDestroyer.lua
@@ -0,0 +1,44 @@
+-- RIP https://www.roblox.com/games/7220965257/Advanced-Launcher-Tycoon-NEW-Beta
+
+if game.PlaceId ~= 7220965257 then return game:GetService("TeleportService"):Teleport(7220965257) end
+
+local LocalPlayer = game:GetService("Players").LocalPlayer
+
+local mode = shared.mode or "wipe"
+
+local function breakall(x)
+    for _,instance in ipairs(x:GetDescendants()) do  
+        if instance:IsA("IntValue") then
+            LocalPlayer.Backpack.ChangeValue:FireServer(instance, -math.huge)
+        end
+        if instance:IsA("StringValue") then
+            LocalPlayer.Backpack.ChangeValue:FireServer(instance, "HOT GAMER SEXX")    
+        end
+        if instance:IsA("BoolValue") then
+            LocalPlayer.Backpack.ChangeValue:FireServer(instance, true)
+        end
+        if instance:IsA("ObjectValue") then
+            LocalPlayer.Backpack.ChangeValue:FireServer(instance, workspace.MyPlayer)
+        end
+    end
+end
+
+local function wipe()
+    for _,LocalPlayer in ipairs(game:GetService("Players"):GetChildren()) do  
+        if LocalPlayer == LocalPlayer then continue end
+
+        LocalPlayer.Backpack.restart1:FireServer()
+    end
+end
+
+if mode == "wipe" then
+    wipe()
+elseif mode == "break" then
+    breakall(game:GetService("Workspace"))
+    wait()
+    breakall(game:GetService("Players"))
+    wait()
+    breakall(game:GetService("ReplicatedStorage"))
+    wait()
+    breakall(game:GetService("StarterPack"))
+end

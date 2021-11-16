f7e3ea52f6ee4dde0c2cd8b561f9e298127180b4
Nameless <Nameless9000@protonmail.com>
2021-11-16 23:08:06 +0000

    Update GetFreeItems.lua

--- Toys Trading/GetFreeItems.lua
+++ Toys Trading/GetFreeItems.lua
@@ -8,21 +8,20 @@ local itemTypes = {
 }
 
 getgenv().freeItem = true
-getgenv().itemType = itemTypes.COLORED
+getgenv().itemType = itemTypes.RAINBOW
 
-if getgenv().ran == true then return end
+if getgenv().ran then return end
 getgenv().ran = true
 
 local rs = game:GetService("ReplicatedStorage")
 local r = rs.Events.Coin.CoinMarketRE
-local m = require(rs.ModuleScripts.ItemConfig)
 local old = nil
 
 old = hookmetamethod(game, "__namecall", function(self, ...)
     local args = {...}
     local method = getnamecallmethod()
 
-    if not checkcaller() and getgenv().freeitem == true and self == r and method == "FireServer" and args[1] == "purchase" then
+    if not checkcaller() and getgenv().freeItem == true and self == r and method == "FireServer" and args[1] == "purchase" then
         x = tostring(args[2])
         x = x:gsub(x:sub(1,1),getgenv().itemType,1)
         rs.Events.PickAndDrop.TakeToolRE:FireServer(tonumber(x))
